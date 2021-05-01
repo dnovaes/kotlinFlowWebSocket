1318c9c37a22f3117da1d981caa7ae719299d52d
dnovaes <novaesdigo@gmail.com>
2021-05-01 15:33:51 -0300

    Implement click to clean zubat from position and change startGame button visibility at the begin of the game

--- app/src/main/java/com/example/flowwebsocket/extensions/LinearLayoutExtensions.kt
+++ app/src/main/java/com/example/flowwebsocket/extensions/LinearLayoutExtensions.kt
@@ -1,13 +1,14 @@
 package com.example.flowwebsocket.extensions
 
 import android.content.Context
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import com.example.flowwebsocket.R
 
-fun LinearLayout.setRowCells(context: Context, onClick: () -> Unit) {
-    for (i in 0..3) {
+fun LinearLayout.setRowCells(context: Context, rowPos: Int, onClick: (View, Int, Int) -> Unit) {
+    for (columnPos in 0..3) {
         val newImageView = Button(context)
         val params = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
@@ -17,7 +18,7 @@ fun LinearLayout.setRowCells(context: Context, onClick: () -> Unit) {
         newImageView.layoutParams = params
         newImageView.background = ContextCompat.getDrawable(context, R.drawable.imageview_border_background)
         newImageView.setOnClickListener {
-            onClick.invoke()
+            onClick.invoke(it, rowPos, columnPos)
         }
         this.addView(newImageView)
     }
--- app/src/main/java/com/example/flowwebsocket/features/roomflow/MainActivity.kt
+++ app/src/main/java/com/example/flowwebsocket/features/roomflow/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.flowwebsocket.features.roomflow
 
-import android.content.Context
-import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.format.Formatter
+import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.flowwebsocket.R
 import com.example.flowwebsocket.databinding.ActivityMainBinding
 import com.example.flowwebsocket.extensions.setRowCells
-import com.example.flowwebsocket.socket.log
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import java.lang.Integer.parseInt
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -24,18 +20,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Timber.plant(Timber.DebugTree())
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initComponents()
         setEvents()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        val wifiMan: WifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        log("ipd ${Formatter.formatIpAddress(wifiMan.connectionInfo.ipAddress)}")
     }
 
     override fun onDestroy() {
@@ -45,22 +33,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun initComponents() {
         this.title = getString(R.string.room_flow_screen_title)
-        setupScreenLayout {
+        setupScreenLayout { view, rowPos, columnPos ->
             val counter = parseInt(binding.counterIdentifier.text.toString())
             binding.counterIdentifier.text = (counter+1).toString()
+            view.background = ContextCompat.getDrawable(
+                applicationContext,
+                R.drawable.imageview_border_background
+            )
         }
     }
 
-    private fun setupScreenLayout(onClick: () -> Unit) {
+    private fun setupScreenLayout(onClick: (View, Int, Int) -> Unit) {
         binding.helloRoom.text = getString(R.string.title_main_activity)
-        binding.row1.setRowCells(this.applicationContext, onClick)
-        binding.row2.setRowCells(this.applicationContext, onClick)
-        binding.row3.setRowCells(this.applicationContext, onClick)
-        binding.row4.setRowCells(this.applicationContext, onClick)
-        binding.row5.setRowCells(this.applicationContext, onClick)
+        binding.row1.setRowCells(this.applicationContext, 0, onClick)
+        binding.row2.setRowCells(this.applicationContext, 1, onClick)
+        binding.row3.setRowCells(this.applicationContext, 2, onClick)
+        binding.row4.setRowCells(this.applicationContext, 3, onClick)
+        binding.row5.setRowCells(this.applicationContext, 4, onClick)
         binding.btStartGame.setOnClickListener {
             lifecycleScope.launch {
                 viewModel.startGame().collect {
+                    binding.btStartGame.visibility = View.GONE
                     showMob(it.posX, it.posY)
                 }
             }
@@ -70,14 +63,10 @@ class MainActivity : AppCompatActivity() {
     private fun setEvents() {
         lifecycleScope.launch {
 /*
-            viewModel.mockedRealTimePositions().collect { roomData ->
-                binding.helloRoom.text = "${roomData.name}: posX: ${roomData.posX}, posY: ${roomData.posY}"
-                viewModel.move(posX = 90, posY = 90)
-            }
-*/
             viewModel.onMovements().collect {
                 binding.helloRoom.text = "Receiving movement at: ${it.posX}, ${it.posY}"
             }
+*/
         }
     }
 
